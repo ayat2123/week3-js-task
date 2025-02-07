cb931b8cc72141c9f8d2ada69d926c7a02c2e909
mahdi <mahdi.amini8894@gmail.com>
2025-02-07 20:09:22 +0330

    sorted number list

--- app.js
+++ app.js
@@ -10,3 +10,8 @@ const nestedarray= [1,[2,[3,[4]],5]]
 const flatarray=nestedarray.flat((Infinity))
 console.log(flatarray)
 
+
+//sorted list
+const unsortnumber=[1,8,11,5,7,19,2,4,3,6,9,10]
+const sortednumber=unsortnumber.sort((a,b)=>(a-b))
+console.log(sortednumber)
